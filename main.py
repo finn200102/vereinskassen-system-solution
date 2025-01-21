bbe93175d08a5853f39ebf05751be08093bb50a1
Finn <112250336+finn200102@users.noreply.github.com>
2025-01-22 00:32:56 +0100

    WIP: add LoginView to root

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 """This is the main of a treasury managmetn system"""
 __author__ = "7157747, Gellien, 8425470, Heidusch"
 import tkinter as tk
+from src.views.login_window import LoginView
 
 
 def main():
@@ -9,6 +10,8 @@ def main():
     root = tk.Tk()
     root.title("Treasury management system")
     root.geometry('500x500')
+    login_view = LoginView(root)
+    login_view.pack()
     root.mainloop()
 
 if __name__ == "__main__":
