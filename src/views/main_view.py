c2f77d0bd631208a38716b3e0667de53d5fd21f6
Finn <112250336+finn200102@users.noreply.github.com>
2025-01-22 01:17:46 +0100

    WIP: add on_login_success

--- src/views/main_view.py
+++ src/views/main_view.py
@@ -4,13 +4,16 @@ import tkinter as tk
 from tkinter import ttk
 from src.views.base_view import BaseView
 from src.views.login_window import LoginView
+from src.views.treasurer_window import TreasurerView
 
 class MainView(ttk.Frame):
-    """The View for the Basewindow"""
+    """The View for the Mainwindow"""
     def __init__(self, master):
+        """Initialize the MainView"""
         super().__init__(master)
         self.master = master
         self.current_view = None
+        self.role = 0 # for testing 0 is treasurer
         self.setup_ui()
 
     def setup_ui(self):
@@ -19,7 +22,16 @@ class MainView(ttk.Frame):
 
     def show_login(self):
         """Show the login View."""
-        self.current_view = LoginView(self)
+        self.current_view = LoginView(self, self.on_login_success)
         self.current_view.pack()
+
+    def on_login_success(self):
+        """Shows the dashboard on login"""
+        if self.current_view:
+            self.current_view.destroy()
+        if self.role == 0:
+            self.current_view = TreasurerView(self)
+            self.current_view.pack()
+        
         
         
