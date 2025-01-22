c46d390d01af2cb4954a982534a492bee68aba5e
Finn <112250336+finn200102@users.noreply.github.com>
2025-01-22 17:15:56 +0100

    WIP: Tests for DataHanlder for transactions

--- tests/TestDataHandler.py
+++ tests/TestDataHandler.py
@@ -3,7 +3,9 @@ __author__ = "7157747, Gellien, 8425470, Heidusch"
 from src.utils.data_handler import DataHandler
 from src.models.user import User
 from src.models.account import Account
+from src.models.transaction import Transaction
 from src.models.user_role import UserRole
+from datetime import datetime
 
 
 def test_add_user(data_handler: DataHandler):
@@ -32,6 +34,23 @@ def test_add_account(data_handler: DataHandler):
     assert data_handler.accounts[0] == account
     print("Test add_account has passed")
 
+def test_add_transaction(data_handler: DataHandler):
+    """
+    Test save_data with a transaction.
+
+    Args:
+        data_handler(DataHandler): The data handler
+    """
+    user1 = data_handler.load_data("user", "franz")
+    user2 = data_handler.load_data("user", "hanz")
+    account1 = Account("abc1", "sport", 20.0, user1)
+    account2 = Account("abc2", "running", 30.0, user2)
+    transaction = Transaction("ab", datetime.now(), 5.0, "move",
+                              account1, account2, "asdf")
+    data_handler.save_data(transaction)
+    assert data_handler.transactions[0] == transaction
+    print("Test add_transaction has passed")
+
 
 def test_load_user(data_handler: DataHandler):
     """
@@ -53,9 +72,19 @@ def test_load_account(data_handler: DataHandler):
     Args:
         data_handler(DataHandler): The data handler
     """
-    user = data_handler.load_data("user", "franz")
-    assert data_handler.load_data("user", "franz") == user
+    assert data_handler.load_data("account", "abc").account_id == "abc"
     print("Test load_account has passed")
+
+
+def test_load_transaction(data_handler: DataHandler):
+    """
+    Test load_data with a transaction.
+
+    Args:
+        data_handler(DataHandler): The data handler
+    """
+    assert data_handler.load_data("transaction", "ab").transaction_id == "ab"
+    print("Test load_transaction has passed")
 
 
 
@@ -76,12 +105,14 @@ def test_export_import(data_handler: DataHandler, users_file, accounts_file, tra
         
     data_handler.export_to_csv()
     data_handler.users = []
+    data_handler.accounts = []
+    data_handler.transactions = []
     data_handler.import_from_csv()
 
 
     assert data_handler.load_data("user", "franz").username == "franz"
-    user = data_handler.load_data("user", "franz")
-    assert data_handler.load_data("user", "franz") == user
+    assert data_handler.load_data("account", "abc").account_id == "abc"
+    assert data_handler.load_data("transaction", "ab").transaction_id == "ab"
 
 
     print("Test import and export passed")
@@ -109,6 +140,10 @@ def main():
     test_add_account(data_handler)
     test_load_account(data_handler)
 
+    # test transaction
+    test_add_transaction(data_handler)
+    test_load_transaction(data_handler)
+    
     test_export_import(data_handler, users_file, accounts_file, transactions_file)
     
    
