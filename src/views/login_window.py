93e6cd5877fb07285459f70d449c61f97fde24c8
Finn <112250336+finn200102@users.noreply.github.com>
2025-01-22 01:17:00 +0100

    WIP: add login_callback to Mainwindow

--- src/views/login_window.py
+++ src/views/login_window.py
@@ -7,18 +7,46 @@ from src.views.base_view import BaseView
 class LoginView(BaseView):
     """The View for the Loginwindow"""
 
+    def __init__(self, parent, on_login_callback):
+        self.on_login_callback = on_login_callback
+        super().__init__(parent)
+        self.parent = parent
+        
     def setup_ui(self):
         """Setup the ui."""
-        username_label = ttk.Label(self, text="Username:")
-        username_entry = ttk.Entry(self)
-        password_label = ttk.Label(self, text="Password:")
-        password_entry = ttk.Entry(self)
+        # clear ui
+        for widget in self.winfo_children():
+            widget.destroy()
+            
+        # create login frame
+        login_frame = ttk.Frame(self)
+        username_label = ttk.Label(login_frame, text="Username:")
+        username_entry = ttk.Entry(login_frame)
+        password_label = ttk.Label(login_frame, text="Password:")
+        password_entry = ttk.Entry(login_frame)
+        login_button = ttk.Button(login_frame, text="Login", command=self.login)
 
         # pack the widgets
         username_label.pack()
         username_entry.pack()
         password_label.pack()
         password_entry.pack()
+        login_button.pack()
+
+        login_frame.pack()
+
+    def login_valid(self):
+        """Checks if login is valid"""
+        #TODO: implement login validation logic
+        return True
+
+    def login(self):
+        """Login into the MainView"""
+        if self.login_valid():
+            self.on_login_callback()
+            
+            
+        
 
         
         
