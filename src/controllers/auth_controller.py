936357e5b635179e39ba94f14b29b1e77dc5b874
Finn <112250336+finn200102@users.noreply.github.com>
2025-01-27 18:02:20 +0100

    WIP: Add login and get_user_roles

--- src/controllers/auth_controller.py
+++ src/controllers/auth_controller.py
@@ -1,5 +1,6 @@
 """This is the AuthController Module"""
 __author__ = "7157747, Gellien, 8425470, Heidusch"
+from src.models.user_role import UserRole
 
 
 class AuthController():
@@ -17,6 +18,19 @@ class AuthController():
         else:
             print(f"The user {username} does not exist!")
             return None
+
+    def login(self, username, password):
+        """Log into the user"""
+        if self.check_password(username, password):
+            user = self.data_handler.load_data("user", username)
+            return user.role
+        else:
+            return False
+
+    def get_user_roles():
+        """Return list of userroles"""
+        user_role = UserRole()
+        return [user_role.admin, user_role.treasurer, user_role.referee]
         
         
         
