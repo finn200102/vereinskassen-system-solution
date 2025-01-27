6c01c25a226dfb9f1d5cf066ce3bbe56707501fe
Finn <112250336+finn200102@users.noreply.github.com>
2025-01-27 18:02:51 +0100

    WIP: Test login

--- tests/TestAuthController.py
+++ tests/TestAuthController.py
@@ -17,11 +17,26 @@ def test_check_password():
     log = auth_controller.check_password("hanz", "1234")
     assert log == True
     print("Check password test has passed.")
+
+
+def test_login():
+    """Test the login method"""
+    users_file = "tests/files/users_file.csv"
+    accounts_file = "tests/files/accounts_file.csv"
+    transactions_file = "tests/files/transactions_file.csv"
+    data_handler = DataHandler(users_file, accounts_file, transactions_file)
+    user = User("hanz", "1234", UserRole.admin, "sport")
+    data_handler.save_data(user)
+    auth_controller = AuthController(data_handler)
+    log = auth_controller.login("hanz", "1234")
+    assert log == user.role
+    print("Login test has passed.")
     
 
 def main():
     """This is the main function."""
     test_check_password()
+    test_login()
 
 if __name__ == "__main__":
     main()
