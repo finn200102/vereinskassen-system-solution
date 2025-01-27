78c5b041789c44756bf6e278814c10eaffb637c3
a_hsch <deine.Mutter@gmail.com>
2025-01-27 19:45:03 +0100

    add_treasurer_view

--- src/views/treasurer_window.py
+++ src/views/treasurer_window.py
@@ -16,4 +16,24 @@ class TreasurerView(ttk.Frame):
     def setup_ui(self):
         """Setup the ui."""
         label = ttk.Label(self, text="Treasurer Window")
+        current_balance_label = ttk.Label(label, text="Current Balance:")
+        current_balance = ttk.Label(label, text="00.00")
+        transaction_amount_label = ttk.Label(label, text="Enter your transfer amount:")
+        transaction_amount = ttk.Entry(label)
+        deposit_button = ttk.Button(label, text="deposit")
+        withdraw_button = ttk.Button(label, text="withdraw")
+        transfer_button = ttk.Button(label, text="transfer")
+        transaction_history_label = ttk.Label(label, text="Transaction History:")
+        transaction_history = ttk.Treeview(label)
+
+        current_balance_label.pack()
+        current_balance.pack()
+        transaction_amount_label.pack()
+        transaction_amount.pack()
+        deposit_button.pack()
+        withdraw_button.pack()
+        transfer_button.pack()
+        transaction_history_label.pack()
+        transaction_history.pack() 
+
         label.pack()
