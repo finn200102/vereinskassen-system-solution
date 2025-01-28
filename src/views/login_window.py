6d007ea6ffc919cef087e1480bcda2c283072f4a
Finn <112250336+finn200102@users.noreply.github.com>
2025-01-28 12:57:13 +0100

    WIP: Add login authentication

--- src/views/login_window.py
+++ src/views/login_window.py
@@ -7,8 +7,11 @@ from src.views.base_view import BaseView
 class LoginView(BaseView):
     """The View for the Loginwindow"""
 
-    def __init__(self, parent, on_login_callback):
+    def __init__(self, parent, on_login_callback, auth_controller):
         self.on_login_callback = on_login_callback
+        self.auth_controller = auth_controller
+        self.username_entry = None
+        self.password_entry = None
         super().__init__(parent)
         self.parent = parent
         
@@ -21,29 +24,33 @@ class LoginView(BaseView):
         # create login frame
         login_frame = ttk.Frame(self)
         username_label = ttk.Label(login_frame, text="Username:")
-        username_entry = ttk.Entry(login_frame)
+        self.username_entry = ttk.Entry(login_frame)
         password_label = ttk.Label(login_frame, text="Password:")
-        password_entry = ttk.Entry(login_frame)
+        self.password_entry = ttk.Entry(login_frame)
         login_button = ttk.Button(login_frame, text="Login", command=self.login)
 
         # pack the widgets
         username_label.pack()
-        username_entry.pack()
+        self.username_entry.pack()
         password_label.pack()
-        password_entry.pack()
+        self.password_entry.pack()
         login_button.pack()
 
         login_frame.pack()
 
     def login_valid(self):
         """Checks if login is valid"""
-        #TODO: implement login validation logic
-        return True
+        log = self.auth_controller.login(self.username_entry.get(), self.password_entry.get())
+        if log:
+            return log
+        else:
+            return False
+
 
     def login(self):
         """Login into the MainView"""
         if self.login_valid():
-            self.on_login_callback()
+            self.on_login_callback(self.login_valid())
             
             
         
